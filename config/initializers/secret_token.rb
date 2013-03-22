696e3a293fb078512089d74f0284d07ee07f8d7c
sleepingkingstudios <merlin@sleepingkingstudios.com>
2013-03-22 00:54:21 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picasa::Application.config.secret_token = '2fca9f9ad68ded384759c6e9a8265a285eb502e600e4650f2b9e65a4d90991d7e581dc99985866363b039af86be7cf28a73a34363fa131b8968a7d2d48f2e8c4'
